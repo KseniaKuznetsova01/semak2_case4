92b8aaa05044d780e971faca62267789aa834afa
KatyaPanukova <55399352+KatyaPanukova@users.noreply.github.com>
2020-03-21 07:33:26 +0300

    Update case.py

--- case.py
+++ case.py
@@ -1,10 +1,12 @@
 import re
+
 spisok = []
 list_1 = []
 list_2 = []
 list_3 = []
 first_words2 = []
 first_words3 = []
+
 
 def first(list_1, list_2):
     with open('input.txt', 'r+') as file:
@@ -16,19 +18,21 @@ def first(list_1, list_2):
         # print(spisok)  # 1-2 пункты задания
         for i in spisok:
             list_1.append(re.split(' ', i[:-1]))
-            list_2.append(list(filter(None,  re.split('\W', i))))
+            list_2.append(list(filter(None, re.split('\W', i))))
 
     return list_1, list_2  # со знаками препинания, чисто слова и ни чего больше
+
+
 first(list_1, list_2)
 
 list_of_words = []
 for j in list_2:
     for a in j:
         list_of_words.append(a)
-print(list_of_words)         # список слов по порядку
-words = set(list_of_words)   # не повторяющиеся слова
+print(list_of_words)  # список слов по порядку
+words = set(list_of_words)  # не повторяющиеся слова
 print(words)
-first_words = []                 # стартовые слова ,заглавные
+first_words = []  # стартовые слова ,заглавные
 for q in words:
     if q[0] == q[0].upper():
         first_words.append(q)
@@ -39,9 +43,11 @@ for n in range(len(list_of_words)):
             pass
 for i in range(len(first_words)):
     for t in range(len(list_of_words)):
-        if list_of_words[t] == first_words[i] and list_of_words[t+1] != first_words[i] and list_of_words[t+1][0] not in 'QWERTYUIOPASDFGHJKLZXCVBNM':
-            first_words2.append(list_of_words[t+1])
-        elif list_of_words[t] == first_words[i] and list_of_words[t+1] != first_words[i] and list_of_words[t+1][0] in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+        if list_of_words[t] == first_words[i] and list_of_words[t + 1] != first_words[i] and list_of_words[t + 1][
+            0] not in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+            first_words2.append(list_of_words[t + 1])
+        elif list_of_words[t] == first_words[i] and list_of_words[t + 1] != first_words[i] and list_of_words[t + 1][
+            0] in 'QWERTYUIOPASDFGHJKLZXCVBNM':
             first_words2.append('.')
 print(first_words2)
 
@@ -50,11 +56,16 @@ for i in range(len(first_words2)):
         for t in range(len(list_of_words)):
             if list_of_words[t] == first_words2[i] and list_of_words[t + 1] != first_words2[i] and \
                     list_of_words[t + 1][0] not in 'QWERTYUIOPASDFGHJKLZXCVBNM':
-                        first_words3.append(list_of_words[t + 1])
+                first_words3.append(list_of_words[t + 1])
             elif list_of_words[t] == first_words2[i] and list_of_words[t + 1] != first_words2[i] and \
                     list_of_words[t + 1][0] in 'QWERTYUIOPASDFGHJKLZXCVBNM':
-                        first_words3.append('.')
+                first_words3.append('.')
     else:
         first_words3.append('.')
 
 print(first_words3)
+list = []
+for i in first_words:
+    for j in first_words2:
+        list.append({i:j})
+print(list)
