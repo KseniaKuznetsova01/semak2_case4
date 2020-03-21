23ed35d306f0834a1559d2198826a251b63fab8b
KseniaKuznetsova01 <ksenks.one@gmail.com>
2020-03-21 11:18:25 +0700

    part 2

--- case.py
+++ case.py
@@ -1,23 +1,60 @@
 import re
-import string
-lines_1 = []
-with open('input.txt', 'r+') as input_file:
-    text = input_file.read()
-    for line in input_file.readlines():
-        pr = text.find(' ')
+spisok = []
+list_1 = []
+list_2 = []
+list_3 = []
+first_words2 = []
+first_words3 = []
 
+def first(list_1, list_2):
+    with open('input.txt', 'r+') as file:
+        lines = file.readlines()
+        for j in lines:
+            line = j[:-1]
+            line += ' '
+            spisok.append(line)
+        # print(spisok)  # 1-2 пункты задания
+        for i in spisok:
+            list_1.append(re.split(' ', i[:-1]))
+            list_2.append(list(filter(None,  re.split('\W', i))))
 
+    return list_1, list_2  # со знаками препинания, чисто слова и ни чего больше
+first(list_1, list_2)
 
-with open('output.txt', 'w+') as output_file:
+list_of_words = []
+for j in list_2:
+    for a in j:
+        list_of_words.append(a)
+print(list_of_words)         # список слов по порядку
+words = set(list_of_words)   # не повторяющиеся слова
+print(words)
+first_words = []                 # стартовые слова ,заглавные
+for q in words:
+    if q[0] == q[0].upper():
+        first_words.append(q)
+print(first_words)
+for n in range(len(list_of_words)):
+    for ken in first_words:
+        if list_of_words[n] == ken:
+            pass
+for i in range(len(first_words)):
+    for t in range(len(list_of_words)):
+        if list_of_words[t] == first_words[i] and list_of_words[t+1] != first_words[i] and list_of_words[t+1][0] not in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+            first_words2.append(list_of_words[t+1])
+        elif list_of_words[t] == first_words[i] and list_of_words[t+1] != first_words[i] and list_of_words[t+1][0] in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+            first_words2.append('.')
+print(first_words2)
 
-    for i in range(len(text)):
-        if text[i] in string.ascii_letters or text[i] in string.digits or \
-                text[i] in 'ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ' or text[i] in '.,?!':
-            output_file.writelines(text[i])
+for i in range(len(first_words2)):
+    if first_words2[i] != '.':
+        for t in range(len(list_of_words)):
+            if list_of_words[t] == first_words2[i] and list_of_words[t + 1] != first_words2[i] and \
+                    list_of_words[t + 1][0] not in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+                        first_words3.append(list_of_words[t + 1])
+            elif list_of_words[t] == first_words2[i] and list_of_words[t + 1] != first_words2[i] and \
+                    list_of_words[t + 1][0] in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+                        first_words3.append('.')
+    else:
+        first_words3.append('.')
 
-    print(text)
-    line = input_file.readlines()
-
-    for k in line:
-        lines_1.append(list(filter(None, re.split('\W', k))))
-    print()
+print(first_words3)
--- test.py
+++ test.py
@@ -8,8 +8,7 @@ with open('input.txt', 'r+') as file:
     for j in lines:
         line = j[:-1]
         line += ' '
-        spisok.append(line)
-    print(spisok)  # 1-2 пункты задания
+     print(spisok)  # 1-2 пункты задания
     for i in spisok:
         list_1.append(re.split(' ', i[:-1]))
         list_2.append(list(filter(None,  re.split('\W', i))))
